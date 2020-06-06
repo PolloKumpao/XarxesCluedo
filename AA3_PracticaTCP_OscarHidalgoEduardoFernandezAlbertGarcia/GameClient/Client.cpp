4fa1702b6e3cc534375e8cbbb282f03601743780
Albert Garcia Mestre <albert.garcia.2@enti.cat>
2020-06-06 14:07:38 +0200

    Organizando codigo

---
+++ AA3_PracticaTCP_OscarHidalgoEduardoFernandezAlbertGarcia/GameClient/Client.cpp
@@ -0,0 +1,577 @@
+#pragma once
+#include <PlayerInfo.h>
+#include <SFML\Network.hpp>
+#include<thread>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fcntl.h>
+#include "Graphics.h"
+
+sf::Packet comando;
+int comandClient = 0;
+int comandServer = 0;
+
+sf::TcpSocket socket;
+std::string prueba = "Prueba conexion";
+sf::Packet pack;
+
+PlayerInfo player;
+PlayerInfo playerInfo;
+Graphics g;
+
+
+
+
+
+
+sf::Socket::Status status = socket.connect("localhost", 50000, sf::milliseconds(15.f));
+sf::Packet packetMano;
+std::vector<carta> mano;
+std::vector<PlayerInfo> players;
+int cartaRepartida = 0;
+int size = 0;
+int habitacionInicial[3];
+sf::Packet packHabitacion;
+int numPlayers = 0;
+int id = 0;
+bool pista = false;
+int dados = 0;
+int tipoPista = 0;
+int n = 0;
+
+estados estadoPlayer = WAIT;
+sf::Event event;
+bool end_game = false;
+enum HEAD {
+	START, LANZADADOS, ELEGIRPISTA, REVELARPISTA, MOVIMIENTO, NUEVAPOSICION, DEDUCCION, ENVIOSUPOSICION,
+	DESMENTIRCS, DESMENTIRSC, NADIE, DESMENTIDO, DESMENTIDOGENERAL, RESOLVERCS, RESOLVERSC, RESOLUCIONCORRECTA
+};
+
+HEAD c;
+
+
+
+void inicioPartida()
+{
+
+	if (status != sf::Socket::Done)
+	{
+		std::cout << "Error al establecer conexion\n";
+		exit(0);
+	}
+	else
+	{
+		std::cout << "Introduce tu Nick:" << std::endl;
+		//cambios por aqui
+		std::string name;
+		std::cin >> name;
+		sf::Packet packNombre;
+		packNombre << name;
+		socket.send(packNombre);
+		sf::Packet packId;
+		socket.receive(packId);
+		packId >> id;
+		std::cout << "Se ha establecido conexion\n";
+		std::cout << "Tu id de jugador es: " << id << std::endl;
+	}
+
+	std::string str = "hola";
+
+	do
+	{
+		socket.receive(packetMano);
+		packetMano >> size;
+
+		std::cout << size << std::endl;
+		for (int i = 0; i < size; i++)
+		{
+			packetMano >> cartaRepartida;
+
+			switch (cartaRepartida)
+			{
+			case 1:
+				mano.push_back((carta(PERSONAJE, "Amapola", 1)));
+				break;
+			case 2:
+				mano.push_back((carta(PERSONAJE, "Rubio", 2)));
+				break;
+			case 3:
+				mano.push_back((carta(PERSONAJE, "Orquidea", 3)));
+				break;
+			case 4:
+				mano.push_back((carta(PERSONAJE, "Celeste", 4)));
+				break;
+			case 5:
+				mano.push_back((carta(PERSONAJE, "Prado", 5)));
+				break;
+			case 6:
+				mano.push_back((carta(PERSONAJE, "Mora", 6)));
+				break;
+			case 7:
+				mano.push_back((carta(ARMA, "Candelabro", 7)));
+				break;
+			case 8:
+				mano.push_back((carta(ARMA, "Cuerda", 8)));
+				break;
+			case 9:
+				mano.push_back((carta(ARMA, "Puñal", 9)));
+				break;
+			case 10:
+				mano.push_back((carta(ARMA, "Pistola", 10)));
+				break;
+			case 11:
+				mano.push_back((carta(ARMA, "Tuberia de Plomo", 11)));
+				break;
+			case 12:
+				mano.push_back((carta(ARMA, "Herramienta", 12)));
+				break;
+			case 13:
+				mano.push_back((carta(SALA, "Biblioteca", 13)));
+				break;
+			case 14:
+				mano.push_back((carta(SALA, "Cocina", 14)));
+				break;
+			case 15:
+				mano.push_back((carta(SALA, "Billar", 15)));
+				break;
+			case 16:
+				mano.push_back((carta(SALA, "Baile", 16)));
+				break;
+			case 17:
+				mano.push_back((carta(SALA, "Invernadero", 17)));
+				break;
+			case 18:
+				mano.push_back((carta(SALA, "Comedor", 18)));
+				break;
+			case 19:
+				mano.push_back((carta(SALA, "Vestibulo", 19)));
+				break;
+			case 20:
+				mano.push_back((carta(SALA, "Salon", 20)));
+				break;
+			case 21:
+				mano.push_back((carta(SALA, "Estudio", 21)));
+				break;
+			default:
+				break;
+			}
+		}
+		for (int i = 0; i < mano.size(); i++)
+		{
+			std::cout << mano[i].nombre << std::endl;
+		}
+
+
+		socket.receive(packHabitacion);
+		packHabitacion >> numPlayers;
+
+		int hab = 0;
+		for (int i = 0; i < numPlayers; ++i)
+		{
+
+			packHabitacion >> hab;
+			switch (hab)
+			{
+			case 1:
+				player.habitacion = BILLAR;
+				player.x = 14;
+				player.y = 5;
+				break;
+			case 2:
+				player.habitacion = BIBLIOTECA;
+				player.x = 24;
+				player.y = 5;
+				break;
+			case 3:
+				player.habitacion = COCINA;
+				player.x = 5;
+				player.y = 25;
+				break;
+			case 4:
+				player.habitacion = BAILE;
+				player.x = 4;
+				player.y = 14;
+				break;
+			case 5:
+				player.habitacion = INVERNADERO;
+				player.x = 4;
+				player.y = 5;
+				break;
+			case 6:
+				player.habitacion = COMEDOR;
+				player.x = 20;
+				player.y = 25;
+				break;
+			case 7:
+				player.habitacion = VESTIBULO;
+				player.x = 35;
+				player.y = 10;
+				break;
+			case 8:
+				player.habitacion = SALON;
+				player.x = 35;
+				player.y = 26;
+				break;
+			case 9:
+				player.habitacion = ESTUDIO;
+				player.x = 36;
+				player.y = 5;
+				break;
+			}
+			g.playerX = player.x;
+			g.playerY = player.y;
+			packHabitacion >> player.id;
+			players.push_back(player);
+		}
+		for (int i = 0; i < numPlayers; ++i)
+		{
+			std::cout << "El jugador " << players[i].id << " comienza en la habitacion " << players[i].habitacion << std::endl;
+		}
+		str = "Fin Inicio";
+		/*std::getline(std::cin, str);
+		sf::Packet packet;
+		packet << str;
+		status = socket.send(packet);
+		if (status != sf::Socket::Done)
+		{
+		std::cout << "Error al enviar\n";
+		}*/
+		std::cout << std::endl;
+	} while (str != "Fin Inicio");
+}
+
+void turno()
+{
+
+	comando >> dados >> pista >> tipoPista;
+	g.movements = dados;
+	std::cout << "Has sacado un " << dados;
+	std::string pistaElegir;
+
+	if (!pista)
+	{
+		std::cout << " y no tienes pista." << std::endl;
+	}
+
+	else
+	{
+
+
+		std::cout << " tienes pista de " << std::endl;
+
+		tipoPista = 0;
+		switch (tipoPista)
+		{
+		case 0:
+			std::cout << "ARMA. Elige entre Candelabro | Cuerda | Punal | Pistola | Tuberia | Herramienta." << std::endl;
+			while (pistaElegir != "Candelabro" && pistaElegir != "Cuerda" && pistaElegir != "Punal" && pistaElegir != "Pistola" && pistaElegir != "Tuberia" && pistaElegir != "Herramienta")
+			{
+				std::cout << "Introduce el nombre correcto del arma que elijas" << std::endl;
+				std::cin >> pistaElegir;
+			}
+			break;
+
+		case 1:
+			std::cout << "SALA. Elige entre Biblioteca | Cocina | Billar | Baile | Invernadero | Comedor | Salon | Estudio." << std::endl;
+			while (pistaElegir != "Biblioteca" && pistaElegir != "Cocina" && pistaElegir != "Billar" && pistaElegir != "Baile" && pistaElegir != "Invernadero" && pistaElegir != "Comedor" && pistaElegir != "Salon" && pistaElegir != "Estudio")
+			{
+				std::cout << "Introduce el nombre correcto de la sala que elijas" << std::endl;
+				std::cin >> pistaElegir;
+			}
+			break;
+
+		case 2:
+			std::cout << "PERSONAJE. Elige entre Amapola | Rubio | Orquidea | Celeste | Prado | Mora. " << std::endl;
+			while (pistaElegir != "Amapola" && pistaElegir != "Rubio" && pistaElegir != "Orquidea" &&  pistaElegir != "Celeste" && pistaElegir != "Prado" && pistaElegir != "Mora")
+			{
+				std::cout << "Introduce el nombre correcto del personaje que elijas" << std::endl;
+				std::cin >> pistaElegir;
+			}
+			break;
+		}
+	}
+	std::cout << "Muevete con las flechas. Confirma tu posicion final con 'barra espaciadora' o vuelve a elegir los movimientos con la tecla 'R'. " << std::endl;
+	comando.clear();
+
+	comando << HEAD::ELEGIRPISTA << pistaElegir;
+	comando << pistaElegir;
+
+	socket.send(comando);
+	comando.clear();
+
+
+
+
+	do {
+		//MOVIMIENTO
+		//send posicion 
+
+		if (event.type == sf::Event::KeyPressed)
+			if (event.key.code == sf::Keyboard::K)
+			{
+				estadoPlayer = WAIT;
+			}
+		sf::Packet packPos;
+		packPos << g.playerX << g.playerY;
+		socket.send(packPos);
+		estadoPlayer = WAIT;
+		//ESTADO = WAIT
+	} while (estadoPlayer == TURNO);
+}
+
+std::string habitacion()
+{
+	if (g.playerX < 8 && g.playerY < 10)
+		return "Invernadero";
+	else if (g.playerX > 11 && g.playerX < 18 && g.playerY < 10)
+		return "Billar";
+	else if (g.playerX > 21 && g.playerX < 28 && g.playerY < 10)
+		return "Biblioteca";
+	else if (g.playerX > 31 && g.playerY < 10)
+		return "Estudio";
+	else if (g.playerX < 8 && g.playerY>11 && g.playerY < 18)
+		return "Baile";
+	else if (g.playerX > 29 && g.playerY > 11 && g.playerY < 20)
+		return "Vestibulo";
+	else if (g.playerX < 10 && g.playerX >19 && g.playerY < 30)
+		return "Cocina";
+	else if (g.playerX > 12 && g.playerX < 26 && g.playerY >19)
+		return "Comedor";
+	else if (g.playerX > 29 && g.playerY > 21)
+		return "Salon";
+	else
+		return "NONE";
+}
+
+
+void revelarPista()
+{
+	std::string pistaRevelada;
+	comando >> pistaRevelada;
+	std::cout << pistaRevelada;
+	comando.clear();
+}
+
+void enviarMov()
+{
+	comando.clear();
+	
+	while (event.key.code != sf::Keyboard::Enter)
+	{
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+			break;
+	}
+
+	comando << HEAD::MOVIMIENTO << g.playerX << g.playerY;
+	socket.send(comando);
+	comando.clear();
+
+}
+void deduccion()
+{
+	
+	comando.clear();
+	std::string arma, sala, personaje;
+	do {
+		sala = habitacion();
+		if (sala == "NONE")
+		{
+			std::cout << "Necesitas estar en una sala para hacer una deduccion" << std::endl;
+		}
+
+		else
+		{
+			std::cout << "Haz tu deduccion: " << std::endl;
+			std::cout << "Sala: " << sala << std::endl;
+			std::cout << "ARMA. Elige entre Candelabro | Cuerda | Punal | Pistola | Tuberia | Herramienta." << std::endl;
+			do
+			{
+				std::cout << "Introduce el nombre correcto" << std::endl;
+				std::cin >> arma;
+
+			} while (arma != "Candelabro" && arma != "Cuerda" && arma != "Punal" && arma != "Pistola" && arma != "Tuberia" && arma != "Herramienta");
+
+
+			std::cout << "PERSONAJE. Elige entre Amapola | Rubio | Orquidea | Celeste | Prado | Mora. " << std::endl;
+			while (personaje != "Amapola" && personaje != "Rubio" && personaje != "Orquidea" &&  personaje != "Celeste" && personaje != "Prado" && personaje != "Mora")
+			{
+				std::cout << "Introduce el nombre correcto" << std::endl;
+				std::cin >> personaje;
+			}
+
+			comando << HEAD::DEDUCCION << arma << sala << personaje;
+			socket.send(comando);
+			comando.clear();
+
+		}
+	} while (sala != "NONE");
+
+}
+void actualizarPos()
+{
+
+	float x, y;
+	x = g.playerX;
+	y = g.playerY;
+	comando >> n >> x >> y;
+	players[n].x = x;
+	players[n].y = y;
+	comando.clear();
+}
+
+void printDeduccion()
+{
+	std::string deduccionPlayer;
+	comando >> deduccionPlayer;
+	std::cout << deduccionPlayer << std::endl;
+	comando.clear();
+}
+
+void opcionesDesmentir()
+{
+	std::string opcion1, opcion2, opcion3;
+	std::string eleccion;
+	comando >> opcion1 >> opcion2 >> opcion3;
+	comando.clear();
+	std::cout << "Escoge la carta que quieras mostrar: " << opcion1 << " | " << opcion2 << " | " << opcion3 << std::endl;
+	do
+	{
+		std::cout << "Introduce una de las opciones" << std::endl;
+		std::cin >> eleccion;
+	} while (eleccion != opcion1 && eleccion != opcion2 && eleccion != opcion3);
+	comando.clear();
+
+	comando << HEAD::DESMENTIDO << eleccion;
+	socket.send(comando);
+}
+void nadie()
+{
+	std::string mensaje;
+	comando >> mensaje;
+	std::cout << mensaje << std::endl;
+	comando.clear();
+}
+void printCarta()
+{
+	std::string carta;
+	comando >> carta;
+	std::cout << "Se muestra la carta: " << carta;
+	comando.clear();
+}
+void recibirComando()
+{
+	while (!end_game)
+	{
+		socket.receive(comando);
+		
+		comando >> comandClient;
+		c=(HEAD)comandClient;
+		switch (c)
+		{
+		case HEAD::LANZADADOS: //Recibes dados
+		{
+			turno();
+			break;
+		}
+
+		case HEAD::REVELARPISTA: //Revelar pista
+		{
+			revelarPista();
+			enviarMov();
+			break;
+		}
+
+		case HEAD::NUEVAPOSICION: //Actualizar pos
+		{
+			actualizarPos();
+			break;
+		}
+
+		case HEAD::DEDUCCION: //Deduccion
+		{
+			deduccion();
+			break;
+		}
+
+		case HEAD::ENVIOSUPOSICION: //Printar deduccion
+		{
+			printDeduccion();
+			break;
+		}
+
+		case HEAD::DESMENTIRSC: //Opciones a elegir
+		{
+			opcionesDesmentir();
+
+			break;
+		}
+
+		case HEAD::NADIE: //Nadie tiene ninguna
+		{
+			nadie();
+			break;
+		}
+
+		case HEAD::DESMENTIDOGENERAL: //Printa la carta escogida por la pantalla de todos los jugadores
+		{
+			printCarta();
+			break;
+		}
+
+		}
+	}
+
+}
+
+
+
+
+
+int main()
+{
+	inicioPartida();
+
+	//recieve;
+	std::thread recieve(recibirComando);
+
+	g.DrawDungeon(socket);
+
+
+
+
+
+
+
+
+	/*wait();
+	turno();*/
+
+
+	//socket.disconnect();
+
+	while (1)
+	{
+
+	}
+
+
+	//sf::Socket::Status status = socket.connect("localhost", 5000, sf::seconds(5.f));
+	//if (status != sf::Socket::Done)
+	//{
+	//	std::cout << "No se ha podido conectar";
+	//}
+	//else {
+	//	std::cout << "Conectado" << std::endl;
+	//}
+
+	//pack << prueba;
+	//socket.send(pack);
+
+	//while (1)
+	//{
+
+	//}
+
+	return 0;
+}
---
+++ AA3_PracticaTCP_OscarHidalgoEduardoFernandezAlbertGarcia/GameServer/Server.cpp
@@ -0,0 +1,598 @@
+#pragma once
+#include <iostream>
+#include <SFML\Graphics.hpp>
+#include <SFML\Network.hpp>
+#include <PlayerInfo.h>
+#include <vector>
+sf::TcpSocket incoming;
+std::list<sf::TcpSocket*> jugadores;
+
+//Funcion por la que el servidor recibe mensajes
+
+enum HEAD {
+	START, LANZADADOS, ELEGIRPISTA, REVELARPISTA, MOVIMIENTO, NUEVAPOSICION, DEDUCCION, ENVIOSUPOSICION,
+	DESMENTIRCS, DESMENTIRSC, NADIE, DESMENTIDO, DESMENTIDOGENERAL, RESOLVERCS, RESOLVERSC, RESOLUCIONCORRECTA
+};
+
+
+HEAD c;
+int dado1;
+int dado2;
+std::list<carta> baraja;
+std::list<carta> crimen;
+//std::list<PlayerInfo> listaPlayers;
+std::vector<PlayerInfo> listaPlayers;
+std::list<sf::TcpSocket*>::iterator jugadorActual;
+int id_jugadorActual;
+sf::Packet comando;
+int comandServer;
+int comandClient;
+bool end_turno = false;
+
+int n = 0; // ID jugador en listaPlayer
+
+
+
+
+void crearBaraja()
+{
+	//6 personajes
+	baraja.push_back(carta(PERSONAJE, "Amapola", 1)); //1
+	baraja.push_back(carta(PERSONAJE, "Rubio", 2)); //2
+	baraja.push_back(carta(PERSONAJE, "Orquidea", 3));//3
+	baraja.push_back(carta(PERSONAJE, "Celeste", 4));//4
+	baraja.push_back(carta(PERSONAJE, "Prado", 5));//5
+	baraja.push_back(carta(PERSONAJE, "Mora", 6));//6
+
+	//6 armas
+	baraja.push_back(carta(ARMA, "Candelabro", 7));//7
+	baraja.push_back(carta(ARMA, "Cuerda", 8));//8
+	baraja.push_back(carta(ARMA, "Punal", 9));//9
+	baraja.push_back(carta(ARMA, "Pistola", 10));//10
+	baraja.push_back(carta(ARMA, "Tuberia", 11));//11
+	baraja.push_back(carta(ARMA, "Herramienta", 12));//12
+
+	//9 habitaciones
+	baraja.push_back(carta(SALA, "Biblioteca", 13));//13
+	baraja.push_back(carta(SALA, "Cocina", 14));//14
+	baraja.push_back(carta(SALA, "Billar", 15));//15
+	baraja.push_back(carta(SALA, "Baile", 16));//16
+	baraja.push_back(carta(SALA, "Invernadero", 17));//17
+	baraja.push_back(carta(SALA, "Comedor", 18));//18
+	baraja.push_back(carta(SALA, "Vestibulo", 19));//19
+	baraja.push_back(carta(SALA, "Salon", 20));//20
+	baraja.push_back(carta(SALA, "Estudio", 21));//21
+
+
+
+}
+void repartirCartas()
+{
+	int r;
+	/*	for (int i = 0; jugadores.size() > i; i++)
+		{
+			listaPlayers.push_back(PlayerInfo());
+		}*/
+
+	for (; baraja.size() != 0;)
+	{
+		for (int i = 0; listaPlayers.size() > i; i++)
+		{
+			std::list<carta>::iterator it = baraja.begin();
+			//std::list<PlayerInfo>::iterator itP = listaPlayers.begin();
+
+			if (baraja.size() > 0)
+			{
+				r = rand() % baraja.size();
+				std::advance(it, r);
+				listaPlayers[i].mano.push_back(*it);
+				baraja.erase(it);
+			}
+
+		}
+	}
+
+
+}
+void enviarRecibirPista()
+{
+
+	std::string pista;
+	//comando.clear();
+	//Recibimos la eleccion de pista del cliente
+	//(*jugadorActual)->receive(comando);
+	comando >> pista;
+	std::cout << "PISTA ELEGIDA: " << pista << std::endl;
+	int i = 0;
+
+	PlayerInfo playerPista;
+	playerPista.name = "null";
+	for (std::list<sf::TcpSocket*>::iterator it = jugadores.begin(); jugadores.end() != it; it++)
+	{
+		for (std::list<carta>::iterator ot = listaPlayers[i].mano.begin(); listaPlayers[i].mano.end() != ot; ot++)
+		{
+			if (pista == (*ot).nombre)
+			{
+				playerPista.name = listaPlayers[i].name;
+				exit;
+			}
+		}
+
+		i++;
+	}
+	comando.clear();
+	std::string pistaFinal;
+	if (playerPista.name == "null")
+	{
+		pistaFinal = "Ningun jugador tiene la carta " + pista;
+		comando << HEAD::REVELARPISTA << pistaFinal;
+	}
+	else
+	{
+		pistaFinal = "El jugador con el Nick:  " + playerPista.name + " tiene la carta" + pista;
+		comando << HEAD::REVELARPISTA << pistaFinal;
+	}
+
+	//posible sendALL
+	for (std::list<sf::TcpSocket*>::iterator it = jugadores.begin(); jugadores.end() != it; it++)
+	{
+		(*it)->send(comando);
+	}
+}
+
+void tirarDados()
+{
+	comando.clear();
+	
+
+	bool pista = false;
+	int r = 0;
+	dado1 = rand() % 6 + 1;
+	dado2 = rand() % 6 + 1;
+
+	if (dado1 == 1 || dado2 == 1)
+	{
+		pista = true;
+		r = rand() % 2;
+	}
+
+
+	dado1 += dado2;
+
+	comando << HEAD::LANZADADOS << dado1 << pista << r;
+
+	(*jugadorActual)->send(comando);
+
+
+
+	/*sf::Packet dadosPack;
+	dadosPack.clear();
+
+	dado1 = rand() % 6 + 1;
+	dado2 = rand() % 6 + 1;
+
+	std::cout << dado1 << "  "<< dado2 << std::endl;
+	if (dado1 == 1 || dado2 == 1)
+		pista = true;
+
+	dado1 += dado2;
+	dadosPack << dado1 << pista;
+	(*jugadorActual)->send(dadosPack);
+
+	if (pista)
+		enviarRecibirPista();*/
+}
+
+void actualizarPos()
+{
+	
+	float x;
+	float y;
+	comando >> x >> y;
+	x = listaPlayers[n].x;
+	y = listaPlayers[n].y;
+
+	comando.clear();
+
+	comando << HEAD::NUEVAPOSICION << n << x << y;
+
+	for (std::list<sf::TcpSocket*>::iterator it = jugadores.begin(); jugadores.end() != it; it++)
+	{
+		(*it)->send(comando);
+	}
+	comando.clear();
+	comando << HEAD::DEDUCCION;
+	(*jugadorActual)->send(comando);
+}
+
+void EnviarAll(sf::Packet p, sf::TcpSocket* id)
+{
+
+	for (std::list<sf::TcpSocket*>::iterator it = jugadores.begin(); jugadores.end() != it; it++)
+	{
+		if (id != (*it))
+		{
+			(*it)->send(p);
+		}
+
+
+	}
+
+}
+
+void Enviar()
+{
+	int i = 0;
+	for (std::list<sf::TcpSocket*>::iterator it = jugadores.begin();jugadores.end() != it; it++)
+	{
+		sf::Packet packMano;
+		std::list<carta>::iterator itM = listaPlayers[i].mano.begin();
+
+		int manosize = listaPlayers[i].mano.size();
+		packMano << manosize;
+		//std::cout << listaPlayers[i].mano.size();
+		for (int j = 0; listaPlayers[i].mano.size() > j; j++)
+		{
+			packMano << itM->numero;
+			//std::cout << itM->numero << std::endl;
+			itM++;
+		}
+		//packMano << 1;
+
+		(*it)->send(packMano);
+		i++;
+	}
+
+}
+
+void asignarHabitacion()
+{
+	int counter1 = 0;
+	int counter = 0;
+	sf::Packet packHabitacion;
+
+	int* r = NULL;
+	r = new int[listaPlayers.size()];
+
+	int tamaño = listaPlayers.size();
+	//Se envia a clientes cuantos jugadores hay en la partida
+	packHabitacion << tamaño;
+
+	for (std::list<sf::TcpSocket*>::iterator it = jugadores.begin(); jugadores.end() != it; it++)
+	{
+		r[counter1] = rand() % 9 + 1;
+		counter1++;
+	}
+
+	for (std::list<sf::TcpSocket*>::iterator it = jugadores.begin(); jugadores.end() != it; it++)
+	{
+		packHabitacion << r[counter] << listaPlayers[counter].id;
+		counter++;
+	}
+
+	for (std::list<sf::TcpSocket*>::iterator it = jugadores.begin(); jugadores.end() != it; it++)
+	{
+		(*it)->send(packHabitacion);
+	}
+}
+
+std::list<carta> crearCrimen(std::list<carta> &b)
+{
+
+	std::list<carta> c;
+	std::list<carta>::iterator it = b.begin();
+
+	int r;
+	r = rand() % 6;
+	std::advance(it, r);
+	c.push_back(*it);
+	b.erase(it);
+
+	r = rand() % 5 + 6;
+	it = b.begin();
+	std::advance(it, r);
+	c.push_back(*it);
+	b.erase(it);
+
+
+	r = rand() % 7 + 12;
+	it = b.begin();
+	std::advance(it, r);
+	c.push_back(*it);
+	b.erase(it);
+	return c;
+
+}
+
+
+void inicioPartida()
+{
+	crearBaraja();
+	crimen = crearCrimen(baraja);
+	repartirCartas();
+	Enviar();
+	asignarHabitacion();
+
+}
+
+
+void setUp(sf::TcpSocket* _client)
+{
+	PlayerInfo player;
+	int _id;
+	_id = listaPlayers.size();
+	sf::Packet packNombre;
+	std::string _name;
+	_client->receive(packNombre);
+	packNombre >> _name;
+	player.name = _name;
+	player.id = _id;
+	sf::Packet packId;
+	packId << _id;
+	_client->send(packId);
+	listaPlayers.push_back(player);
+
+
+
+	std::cout << "LLega el cliente con nick: " << _name;
+}
+
+void ControlServidor()
+{
+	bool running = true;
+	bool inicio = false;
+	// Create a socket to listen to new connections
+	sf::TcpListener listener;
+	sf::Socket::Status status = listener.listen(50000);
+	if (status != sf::Socket::Done)
+	{
+		std::cout << "Error al abrir listener\n";
+		exit(0);
+	}
+	// Create a list to store the future clients
+
+	// Create a selector
+	sf::SocketSelector selector;
+	// Add the listener to the selector
+	selector.add(listener);
+	// Endless loop that waits for new connections
+	while (running)
+	{
+		// Make the selector wait for data on any socket
+		if (selector.wait())
+		{
+			// Test the listener
+			if (selector.isReady(listener))
+			{
+				// The listener is ready: there is a pending connection
+				sf::TcpSocket* client = new sf::TcpSocket;
+				if (listener.accept(*client) == sf::Socket::Done)
+				{
+					// Add the new client to the clients list
+					setUp(client);
+					std::cout << " y con puerto: " << client->getRemotePort() << std::endl;
+					jugadores.push_back(client);
+					// Add the new client to the selector so that we will
+					// be notified when he sends something
+					selector.add(*client);
+				}
+				else
+				{
+					// Error, we won't get a new connection, delete the socket
+					std::cout << "Error al recoger conexion nueva\n";
+					delete client;
+				}
+			}
+			else
+			{
+				// The listener socket is not ready, test all other sockets (the clients)
+				for (std::list<sf::TcpSocket*>::iterator it = jugadores.begin(); it != jugadores.end(); ++it)
+				{
+					sf::TcpSocket& client = **it;
+					if (selector.isReady(client))
+					{
+						// The client has sent some data, we can receive it
+						sf::Packet packet;
+						status = client.receive(packet);
+						if (status == sf::Socket::Done)
+						{
+							std::string strRec;
+							packet >> strRec;
+							std::cout << "He recibido " << strRec << " del puerto " << client.getRemotePort() << std::endl;
+						}
+						else if (status == sf::Socket::Disconnected)
+						{
+							selector.remove(client);
+							std::cout << "Elimino el socket que se ha desconectado\n";
+						}
+						else
+						{
+							std::cout << "Error al recibir de " << client.getRemotePort() << std::endl;
+						}
+					}
+				}
+			}
+		}
+		if (jugadores.size() == 3 && !inicio)
+		{
+			inicioPartida();
+			std::cout << "Empieza la partida: " << std::endl;
+			inicio = true;
+			running = false;
+		}
+	}
+
+}
+
+void desmentir()
+{
+	std::string arma, sala, personaje;
+	int jugadorIzquierda = n + 1;
+	comando >> arma >> sala >> personaje;
+
+	std::string deduccion = "El jugador " + listaPlayers[n].name + " ha hecho la siguiente deduccion: " + personaje + " " + sala + " " + arma;
+	comando.clear();
+	
+	comando << HEAD::ENVIOSUPOSICION << deduccion;
+	for (std::list<sf::TcpSocket*>::iterator it = jugadores.begin(); jugadores.end() != it; it++)
+	{
+		(*it)->send(comando);
+		//std::cout << "Envio deduccion" << std::endl;
+	}
+	comando.clear();
+	
+	comando << HEAD::DESMENTIRSC;
+	int j = 0;
+	bool tieneAlguna = false;
+
+	for (std::list<sf::TcpSocket*>::iterator it = jugadores.begin(); jugadores.end() != it; it++)
+	{
+		if (jugadorActual != (it))
+		{
+
+
+
+			for (std::list<carta>::iterator ot = listaPlayers[j].mano.begin(); listaPlayers[j].mano.end() != ot; ot++)
+			{
+				if ((ot->nombre == arma))
+				{
+					comando << arma;
+					tieneAlguna = true;
+				}
+
+				if ((ot->nombre == sala))
+				{
+					comando << sala;
+					tieneAlguna = true;
+				}
+				if ((ot->nombre == personaje))
+				{
+					comando << personaje;
+					tieneAlguna = true;
+				}
+			}
+
+			if (tieneAlguna)
+			{
+				(*it)->send(comando);
+				break;
+			}
+		}
+		j++;
+	}
+	if (!tieneAlguna)
+	{
+		std::string nadie = "Ningun jugador tiene ninguna de las 3 cartas";
+		for (std::list<sf::TcpSocket*>::iterator it = jugadores.begin(); jugadores.end() != it; it++)
+		{
+			comando.clear();
+			
+			comando << HEAD::NADIE << nadie;
+			(*it)->send(comando);
+
+		}
+	}
+
+}
+
+void enseñarCarta()
+{
+	std::string carta;
+	comando >> carta;
+	comando.clear();
+	
+	comando << HEAD::DESMENTIDOGENERAL << carta;
+	for (std::list<sf::TcpSocket*>::iterator it = jugadores.begin(); jugadores.end() != it; it++)
+	{
+		(*it)->send(comando);
+	}
+}
+
+void recibirPacket()
+{
+	
+	(*jugadorActual)->receive(comando);
+	int temp;
+	
+	comando >> temp;
+	c = (HEAD)temp;
+	std::cout << "He recibido " << comandServer;
+	switch (c)
+	{
+	case HEAD::ELEGIRPISTA:    //Recibir Pista
+		enviarRecibirPista();
+		break;
+	case HEAD::MOVIMIENTO:		//Recibir pos
+		actualizarPos();
+		break;
+	case HEAD::DEDUCCION:     //Recibir deduccion
+		desmentir();
+		break;
+	case HEAD::DESMENTIDO:
+		enseñarCarta();
+		break;
+	}
+}
+
+void turno()
+{
+
+
+	//(*jugadorActual)->send(comando);
+	tirarDados();
+	while (!end_turno)
+	{
+		recibirPacket();
+
+	}
+
+
+}
+
+void partida()
+{
+	std::cout << "Entro en la partida" << std::endl;
+	std::list<sf::TcpSocket*>::iterator j = jugadores.begin();
+	jugadorActual = j;
+
+
+	while (1)
+	{
+		turno();
+		if (j == jugadores.end())
+		{
+			j = jugadores.begin();
+		}
+		else
+		{
+			j++;
+			jugadorActual = j;
+		}
+		if (listaPlayers.size() == n)
+		{
+			n = 0;
+		}
+		else
+		{
+			n++;
+		}
+
+
+
+	}
+}
+
+int main()
+{
+
+	srand(time(NULL));
+	std::cout << "Esperando jugadores... La partida empezara cuando hayan 3 jugadores:  " << std::endl;
+	ControlServidor();
+	partida();
+
+	//tirarDados();
+
+
+
+	return 0;
+}
